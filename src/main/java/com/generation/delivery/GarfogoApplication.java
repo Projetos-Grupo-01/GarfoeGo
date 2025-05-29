aa9519a1de66e424836c3dd4fd14db6f85076a2f
Aylla Scaglia <aylla@scaglia.com.br>
2025-05-29 09:56:19 -0300

    :tada: Commit Inicial

---
+++ src/main/java/com/generation/delivery/GarfogoApplication.java
@@ -0,0 +1,13 @@
+package com.generation.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarfogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarfogoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/delivery/GarfogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarfogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
